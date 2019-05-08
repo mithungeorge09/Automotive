063c402de0cef0925a058b5349251ffcb5cf5a54
mithungeorge09 <50397413+mithungeorge09@users.noreply.github.com>
2019-05-08 15:36:56 +0530

    Update mithungeorge.c

--- mithungeorge.c
+++ mithungeorge.c
@@ -2,21 +2,22 @@
 #include <stdint.h>
 int main()
 {
-  int row, c;
- printf("This is going to print a triangle pattern");
-  
+  int row, c, s;
+  s=5;
+ printf("This is going to print a triangle pattern\n");
+
   for (row = 1; row <= 5; row++)  // Loop to print rows
   {
     for (c = 1; c < 5; c++)  // Loop to print spaces in a row
       printf(" ");
- 
+
     s--;
- 
+
     for (c = 1; c <= 2*row - 1; c++) // Loop to print stars in a row
       printf("*");
- 
+
     printf("\n");
   }
- 
+
   return 0;
 }
